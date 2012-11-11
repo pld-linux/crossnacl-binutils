caf4e83b87722e1a77894163ab8088ed452f6b8b
Elan Ruusamäe <glen@delfi.ee>
2012-11-11 11:31:51 +0200

    - use http url for omahaproxy

--- get-source.sh
+++ get-source.sh
@@ -16,7 +16,7 @@ set -e
 package=nacl-binutils
 repo_url=http://git.chromium.org/native_client/$package.git
 nacl_trunk=http://src.chromium.org/native_client/trunk
-omahaproxy_url=https://omahaproxy.appspot.com
+omahaproxy_url=http://omahaproxy.appspot.com
 specfile=crossnacl-binutils.spec
 
 chrome_channel=${1:-stable}
